6b6da31544acaabe30140503be6392f56f3310b4
DFAsniper <134014156+DFAsniper@users.noreply.github.com>
2026-03-24 12:42:39 -0500

    V2.1: Cleaned config, removed unused polling and timeout settings

--- AutoDumper/scripts/config.lua
+++ AutoDumper/scripts/config.lua
@@ -1,39 +1,17 @@
 -- true = run
 -- false = skip
--- Make sure to changes these depending on what dumps you want ran!!!
---   Default is set to have all 6 set to true! 
-
-
+-- Make sure to change these depending on what dumps you want ran!!!
+-- Default is set to have all 6 set to true!
 
 AutoDumperConfig = {
     INITIAL_DELAY_MS = 15000,
-    POLL_INTERVAL_MS = 2000,
-    DEFAULT_TIMEOUT_MS = 120000,
 
     DUMPERS = {
-        SDK = {
-            ENABLED = true,
-            TIMEOUT_MS = 120000
-        },
-        OBJECTS = {
-            ENABLED = true,
-            TIMEOUT_MS = 120000
-        },
-        UHT = {
-            ENABLED = true,
-            TIMEOUT_MS = 180000
-        },
-        STATIC_MESHES = {
-            ENABLED = true,
-            TIMEOUT_MS = 120000
-        },
-        ACTORS = {
-            ENABLED = true,
-            TIMEOUT_MS = 120000
-        },
-        USMAP = {
-            ENABLED = true,
-            TIMEOUT_MS = 120000
-        }
+        SDK = { ENABLED = true },
+        OBJECTS = { ENABLED = true },
+        UHT = { ENABLED = true },
+        STATIC_MESHES = { ENABLED = true },
+        ACTORS = { ENABLED = true },
+        USMAP = { ENABLED = true }
     }
 }
